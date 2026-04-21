3ac91ad9638e50e1601a15e3db7b7b0c7054f942
Claudia-Medina-UNI <claudia.medinap@uanl.edu.mx>
2026-04-20 21:38:00 -0600

    El principio de mi codigo 1

---
+++ RegistroBackrooms.py
@@ -0,0 +1,7 @@
+class RegistroNivelBackrooms:
+    def _init_(self, numero_nivel, nombre_nivel, clase_seguridad, entidades_totales, habitable):
+        self.__numero_nivel = numero_nivel
+        self.__nombre_nivel = nombre_nivel
+        self.__clase_seguridad = clase_seguridad
+        self.__entidades_totales = entidades_totales
+        self.__habitable = habitable
