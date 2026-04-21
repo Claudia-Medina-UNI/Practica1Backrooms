e22efb28ff2fc8ba09805d45c07c883b5ad81094
Claudia-Medina-UNI <claudia.medinap@uanl.edu.mx>
2026-04-20 21:41:46 -0600

    Añadi los getters y setters en mi code

--- RegistroBackrooms.py
+++ RegistroBackrooms.py
@@ -5,3 +5,28 @@ class RegistroNivelBackrooms:
         self.__clase_seguridad = clase_seguridad
         self.__entidades_totales = entidades_totales
         self.__habitable = habitable
+        
+    def getNumeroNivel(self):
+        return self.__numero_nivel
+    def setNumeroNivel(self, numero_nivel):
+        self.__numero_nivel = numero_nivel
+
+    def getNombreNivel(self):
+        return self.__nombre_nivel
+    def setNombreNivel(self, nombre_nivel):
+        self.__nombre_nivel = nombre_nivel
+
+    def getClaseSeguridad(self):
+        return self.__clase_seguridad
+    def setClaseSeguridad(self, clase_seguridad):
+        self.__clase_seguridad = clase_seguridad
+
+    def getEntidadesTotales(self):
+        return self.__entidades_totales
+    def setEntidadesTotales(self, entidades_totales):
+        self.__entidades_totales = entidades_totales
+
+    def getHabitable(self):
+        return self.__habitable
+    def setHabitable(self, habitable):
+        self.__habitable = habitable
